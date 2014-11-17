ec060ec2efda418d20b95e1562c34df50a46f032
Yerui Yu <yeruidacz@gmail.com>
2014-11-16 23:07:19 -0600

    add footer,header,index

---
+++ footer.php
@@ -0,0 +1,25 @@
+ <div class="footer-clear"></div>
+<footer class="row no-max pad">
+  <ul class="social-links no-bullet">
+    <li><a href="" class="icon icon-twitter"></a></li>
+    <li><a href="" class="icon icon-facebook"></a></li>
+    <li><a href="" class="icon icon-vimeo"></a></li>
+    <li><a href="" class="icon icon-youtube"></a></li>
+    <li><a href="" class="icon icon-linkedin"></a></li>
+    <li><a href="" class="icon icon-github"></a></li>
+    <li><a href="" class="icon icon-flickr"></a></li>
+    <li><a href="" class="icon icon-google"></a></li>
+    <li><a href="" class="icon icon-email"></a></li>
+  </ul>            
+  <p>Copyright 2014</p>
+</footer>
+
+<script src="assets/js/vendor/jquery.js"></script>
+<script src="assets/js/foundation.min.js"></script>
+<script src="assets/js/app.js"></script>
+<script>
+  $(document).foundation();
+</script>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+<p>this is index.php</p>
+
+<?php get_footer(); ?>
